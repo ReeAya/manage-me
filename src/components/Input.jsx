7708b73f88228088795939e65b9599ef86985e2b
reezan_s <reezansmith@gmail.com>
2024-08-26 22:56:58 +0900

    Created NewProject and Input components

---
+++ src/components/Input.jsx
@@ -0,0 +1,8 @@
+export default function Input({ label, textarea, ...props }) {
+  return (
+    <p>
+      <label>{label}</label>
+      {textarea ? <textarea /> : <input {...props} />}
+    </p>
+  );
+}
