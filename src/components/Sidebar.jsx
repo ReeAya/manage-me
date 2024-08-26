5b8064febacadea37880d99bd42542ecb6ebba5b
reezan_s <reezansmith@gmail.com>
2024-08-26 22:38:06 +0900

    Created Sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+export default function Sidebar() {
+  return (
+    <aside>
+      <h2>My projects</h2>
+      <div>
+        <button>+ Add new project</button>
+      </div>
+      <ul></ul>
+    </aside>
+  );
+}
