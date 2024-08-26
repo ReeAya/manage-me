1aec8e2396c1484463e69bd5156f88d3b82e8aa9
reezan_s <reezansmith@gmail.com>
2024-08-26 22:49:03 +0900

    Added style classes to Sidebar component"

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,13 @@
 export default function Sidebar() {
   return (
-    <aside>
-      <h2>My projects</h2>
+    <aside className="w-1/3 px-8 py-16 bg-zinc-500 text-stone-50 md:w-72 rounded-r-xl">
+      <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
+        My projects
+      </h2>
       <div>
-        <button>+ Add new project</button>
+        <button className="px-4 py-2 text-xs md:text-base rounded-md bg-zinc-200 text-zinc-600 hover:bg-zinc-600 hover:text-zinc-100">
+          + Add new project
+        </button>
       </div>
       <ul></ul>
     </aside>
