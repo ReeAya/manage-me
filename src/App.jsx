f1841bfaddd7db5b39e6eb34f6e02175e22591b0
reezan_s <reezansmith@gmail.com>
2024-08-27 10:13:29 +0900

    Added state to handle and manage existing projects and adding new ones

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
 import Sidebar from "./components/Sidebar";
 import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectID: undefined,
+    projects: [],
+  });
+
+  function handleStartAddingProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectID: null,
+      };
+    });
+  }
+
+  let content;
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <NoProjectSelected />
+      <Sidebar onStartAddingProject={handleStartAddingProject} />
+      <NoProjectSelected onStartAddingProject={handleStartAddingProject} />
     </main>
   );
 }
--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,7 +1,7 @@
 import noProjectImage from "../assets/memo.png";
 import Button from "./Button";
 
-export default function NoProjectSelected() {
+export default function NoProjectSelected({ onStartAddingProject }) {
   return (
     <div className="mt-24 text-center w-2/3">
       <img
@@ -16,7 +16,7 @@ export default function NoProjectSelected() {
         Select a project or get started with a new one!
       </p>
       <p className="mt08">
-        <Button>Create New Project</Button>
+        <Button onClick={onStartAddingProject}>Create New Project</Button>
       </p>
     </div>
   );
